04c50c81c4dba18514ca3ffd0606bc659bb14bf0
Eko Kurniadi <eko.kurniadi@majoo.id>
2022-12-07 00:54:39 +0700

    belajar deklarasi variable

---
+++ 04_variable/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    // Deklarasi variabel angka pertama dengan tipe data integer
+    int angka_pertama = 1;
+
+    std::cout << angka_pertama << std::endl;
+    std::cin.get();
+    return 0;
+}
