127f3eb271afc3e1b90af7da617f68f929660e30
Eko Kurniadi <eko.kurniadi@majoo.id>
2022-12-07 01:04:33 +0700

    belajar deklarasi variabel dan memasukkan nilai ke variable melalui terminal

---
+++ 05_deklarasi/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // deklarasi variabel
+    int deklarasi_variabel;
+
+    // mengisi nilai variabel
+    deklarasi_variabel = 1;
+
+    cout << deklarasi_variabel << endl;
+
+    // deklarasi variable kedua
+    int deklarasi_variable_kedua;
+
+    cout << "masukkan nilai :";
+
+    // mengisi nilai dari terminal ke variable kedua
+    cin >> deklarasi_variable_kedua;
+
+    cout << "ini adalah angka yang anda masukkan adalah :";
+    cout << deklarasi_variable_kedua << endl;
+
+    cin.get();
+    return 0;
+}
