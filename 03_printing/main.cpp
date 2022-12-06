9caa30e700ff15f8b4f1b1109883ac581405359b
Eko Kurniadi <eko.kurniadi@majoo.id>
2022-12-07 00:46:11 +0700

    belajar print text ke terminal

---
+++ 03_printing/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+    std::cout << "ini akan ditampilkan di terminal" << std::endl;
+    std::cout << "ini akan ditampilkan di terminal baris ke 2" << std::endl;
+    std::cin.get();
+    return 0;
+}
